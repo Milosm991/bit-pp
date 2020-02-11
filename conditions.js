44a5a58dbc5fdae26132312c8d35f5548695ba1c
BIT Student <classroom@bgit.rs>
2020-02-11 18:28:30 +0100

    prva promena

---
+++ conditions.js
@@ -0,0 +1,100 @@
+/* 2. var result = ""
+var a = 1;
+var b = -2;
+var c = -6;
+var d = 0;
+var e = -1;
+
+if (a > b && a > c && a > d && a > e) {
+    result = a;
+} else if (b > c && b > d && b > e) {
+    result = b;
+} else if (c > d && c > e) {
+    result = c;
+} else if (d > e) {
+    result = d;
+} else {
+    result = e;
+}
+console.log(result);
+
+4. var a = 10;
+var res = ""
+var isNumber = typeof a === "number" && a % 2 === 0;
+
+if (isNumber) {
+        res = a / 2;
+}
+
+else {
+    res = "x"
+}
+console.log(res)
+
+5. var res = "";
+var a = 5;
+var b = 1;
+
+if (a > b) {
+    res = a;
+} else {
+    res = b;
+}
+console.log(res)
+
+7. var res = ""
+var a = 11;
+var b = 13;
+
+if (a > b) {
+    var c = a - b;
+    res = c * 2;
+} else {
+    var c = b - a;
+    res = c;
+}
+console.log(res)
+
+6. var c = 60;
+var far = (9 * c / 5) + 32
+console.log(far)
+
+8. var res = "";
+var a = 4;
+var b = 5;
+
+if (a === b) {
+    var c = a + b;
+    res = c * 3;
+} else {
+    var c = a + b;
+    res = c;
+}
+console.log(res)
+
+
+var a = 68;
+var b = 25;
+var c = a + b;
+var d = a === 50 || b === 50 || c === 50;
+var res = ""
+if (d) {
+    res = "true"
+} else {
+    res = "-"
+}
+console.log(res)
+
+var res = ""
+var a = 100;
+
+if (a >= 20 && a <= 100) {
+    res = "20-100";
+} else if (a > 100 && a <= 400) {
+    res = "100-400"
+} else {
+    res = "-";
+}
+
+console.log(res)*/
+
