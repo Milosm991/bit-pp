6c7d1b991945863063fd2646f98b43f441e3a3ca
Milosm991 <milosmanojlovic91@gmail.com>
2020-02-12 19:25:11 +0100

    druga izmena,array switch

---
+++ Array, switch 12.02/Array.js
@@ -0,0 +1,53 @@
+1.
+/*var months = ["january", "february", "mart", "april", "may", "jun", "july", "august", "september", "october", "november", "december"];
+
+console.log(months[5], months[9], months[0])
+
+2.
+
+var days = ["monday", "tuesday", "sreda", "cetvrtak", "petak", "saturday", "sunday"];
+
+
+console.log(days[6])
+
+3.
+
+var elements = [2, -4, -5, -2, -11];
+
+console.log(elements[1], elements[2], elements[3], elements[4]);
+
+4.
+
+var divisible = [5, 15, -5, 20, 12, 18];
+
+if (divisible[0] % 3 === 0) {
+    console.log(divisible[0])
+}
+if (divisible[1] % 3 === 0) {
+    console.log(divisible[1])
+}
+if (divisible[2] % 3 === 0) {
+    console.log(divisible[2])
+}
+if (divisible[3] % 3 === 0) {
+    console.log(divisible[3])
+}
+if (divisible[4] % 3 === 0) {
+    console.log(divisible[4])
+}
+if (divisible[5] % 3 === 0) {
+    console.log(divisible[5])
+}*/
+
+5.
+
+var a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+var index = a[0][3];
+var third = a[2];
+var secondOfThird = a[2][1];
+console.log(index)
+console.log(third)
+console.log(secondOfThird)
+
+
+
---
+++ Array, switch 12.02/switch.js
@@ -0,0 +1,230 @@
+1.
+
+/*var a = 10;
+var res = ""
+switch (a) {
+    case 1:
+        res = "Monday";
+        break;
+    case 2:
+        res = "Tuesday";
+        break;
+    case 3:
+        res = "Wednesday";
+        break;
+    case 4:
+        res = "Thursday";
+        break;
+    case 5:
+        res = "Friday";
+        break;
+    case 6:
+        res = "Saturday";
+        break;
+    case 7:
+        res = "Sunday";
+        break;
+    default:
+        res = "I don\'t know";
+        break;
+}
+console.log(res)
+
+2.
+
+var a = 10;
+var res = ""
+switch (a) {
+    case 1:
+        res = "Monday";
+        break;
+    case 2:
+        res = "Tuesday";
+        break;
+    case 3:
+        res = "Wednesday";
+        break;
+    case 4:
+        res = "Thursday";
+        break;
+    case 5:
+        res = "Friday";
+        break;
+    case 6:
+        res = "Saturday";
+        break;
+    case 7:
+        res = "Sunday";
+        break;
+    default:
+        res = "Input must be a number between 1 and 7";
+        break;
+}
+console.log(res)
+3.
+
+var a = 7;
+var res = ""
+switch (a) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        res = "It\'s weekday";
+        break;
+    case 6:
+    case 7:
+        res = "It\'s weekend";
+        break;
+    default:
+        res = "Input must be a number between 1 and 7";
+        break;
+}
+console.log(res)
+
+4.
+
+var a = 12;
+var res = ""
+switch (a) {
+    case 1:
+        res = "January";
+        break;
+    case 2:
+        res = "February";
+        break;
+    case 3:
+        res = "Mart";
+        break;
+    case 4:
+        res = "April";
+        break;
+    case 5:
+        res = "May";
+        break;
+    case 6:
+        res = "June";
+        break;
+    case 7:
+        res = "July";
+        break;
+    case 8:
+        res = "August";
+        break;
+    case 9:
+        res = "September";
+        break;
+    case 10:
+        res = "October";
+        break;
+    case 11:
+        res = "November";
+        break;
+    case 12:
+        res = "December"
+        break;
+    default:
+        res = "Input must be a number between 1 and 12";
+        break;
+}
+console.log(res)
+
+5.
+
+var a = 1;
+var res = "";
+switch (a) {
+    case 12:
+    case 1:
+    case 2:
+    case 3:
+        res = "It\'s winter";
+        break;
+    case 4:
+    case 5:
+    case 6:
+        res = "it\'s spring";
+        break;
+    case 7:
+    case 8:
+    case 9:
+        res = "It\'s summer";
+        break;
+    case 10:
+    case 11:
+        res = "It\'s autmun";
+        break;
+    default:
+        res = "Input must be a number between 1 and 12";
+        break;
+}
+console.log(res)
+
+6.
+
+var a = "D";
+var res = "";
+
+switch (a) {
+    case "A":
+        res = "Good job";
+        break;
+    case "B":
+        res = "Pretty good";
+        break;
+    case "C":
+        res = "Passed";
+        break;
+    case "D":
+        res = "Not so good";
+        break;
+    case "F":
+        res = "Failed";
+        break;
+    default:
+        res = "Unknown grade";
+        break;
+}
+console.log(res)
+
+7.
+
+var city = "Moscow";
+var res = ""
+
+switch (city) {
+    case "Beograd":
+    case "Novi Sad":
+    case "Arilje":
+    case "Nis":
+    case "Krusevac":
+        res = "Srbija";
+        break;
+    case "London":
+    case "Liverpool":
+    case "Manchester":
+        res = "England";
+        break;
+    case "Rome":
+    case "Milano":
+    case "Florence":
+    case "Naples":
+        res = "Italy";
+        break;
+    case "Madrid":
+    case "Valencia":
+        res = "Spain";
+        break;
+    case "Paris":
+        res = "France";
+        break;
+    default:
+        res = "Please choose a different city";
+        break;
+}
+console.log(res)*/
+
+8.
+
+
