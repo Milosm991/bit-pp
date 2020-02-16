5407cf5f1d499ec235478676ce625ad65d5144aa
Milosm991 <milosmanojlovic91@gmail.com>
2020-02-16 09:57:17 +0100

    vezbanje

--- Loops 13.02/loops.js
+++ Loops 13.02/loops.js
@@ -50,7 +50,59 @@ var res = "";
 for (var i = 0; i < x.length; i++) {
     res = res + x[i];
 }
-console.log(res)*/
+console.log(res)
+
+5.
+var res="";
+var a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27]];
+for(i=0; i<a.length; i++){
+    res =res+a[i];
+}
+console.log(res);
+
+6.
+var sum=0;
+for(var a=0; a<=20; a++){
+    var b=a*a;
+    sum=sum+b;
+}
+console.log(sum);
+
+7.
+var students=[80, 77, 88, 95, 68];
+var sum=0;
+var avgGrade=1;
+for(i=0; i<students.length; i++){
+       sum=sum+students[i];
+       avgGrade=sum/5;
+}
+console.log(avgGrade);
+if(avgGrade<60){
+    console.log("F");
+}else if(avgGrade<70){
+    console.log("D");
+}else if(avgGrade<80){
+    console.log("C");
+}else if(avgGrade<90){
+    console.log("B");
+}else{
+    console.log("A");
+}*/
+
+8.
+
+for(var a=0; a<=100; a++){
+    if(a%3===0){
+        console.log(a+"Fizz");        
+    }else if(a%5===0&&a%3!==0){
+        console.log(a+"Buzz"); 
+    }    else{
+        console.log(a)
+    }
+    if(a%5===0&&a%3===0){
+        console.log(a+"FizzBuzz")
+    }
+}
 
 
 
