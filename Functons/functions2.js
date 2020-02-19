421b8f73d78e4fc71a0001c232372c8475edf1b7
BIT Student <classroom@bgit.rs>
2020-02-19 17:56:08 +0100

    vezbanjefunc

---
+++ Functons/functions2.js
@@ -0,0 +1,10 @@
+function isInputAString(a) {
+
+    if (typeof a === "string") {
+        res = true;
+    }
+    return res;
+}
+var res = false;
+var c = isInputAString("string")
+console.log(res);
