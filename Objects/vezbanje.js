c3048a4fbea15f77ea5530c904c7e1697e0d2e84
BIT Student <classroom@bgit.rs>
2020-03-02 17:56:37 +0100

    vezba

---
+++ Objects/vezbanje.js
@@ -0,0 +1,15 @@
+
+
+function array(a, b) {
+    "use strict";
+    var newArray = [];
+    for (var i = 0, j = 0; i < a.length, j < b.length; i++ , j++) {
+        newArray.push(a[i]);
+        newArray.push(b[j]);
+    }
+    return newArray
+
+}
+
+var res = array(["a", "b", "c"], [1, 2, 3])
+console.log(res)
